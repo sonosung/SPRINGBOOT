fa0e1991cd550d65f9c412fe66b57c97ba88254f
sonosung <msh071@naver.com>
2024-07-16 10:54:48 +0900

    2024.07.16 교재 chapter2를 위한 프로젝트 생성.

---
+++ guestbook/src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -0,0 +1,14 @@
+package org.zerock.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookApplication.class, args);
+		System.out.println("as");
+	}
+
+}
